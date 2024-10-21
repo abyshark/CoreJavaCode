f638685d21bec26dd3ca87aefa17ebcb0b5b98bf
Abhishek Tripathi <abhishekst.2021@gmail.com>
2024-10-21 10:10:15 +0530

    async folder added

---
+++ src/com/demo/async/DemoAsync1.java
@@ -0,0 +1,8 @@
+package com.demo.async;
+
+public class DemoAsync1 {
+    public static void main(String[] args) {
+        System.out.println("This is the program for basic async in java!!!!");
+
+    }
+}
